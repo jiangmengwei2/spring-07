bd5b56cfa81d916d5e307927791ee4cd07d66b27
jiangmengwei2 <3236618935@qq.com>
2018-02-24 17:30:58 +0800

    first

---
+++ src/练习/猫.java
@@ -0,0 +1,38 @@
+package Á·Ï°;
+import Á·Ï°2.Cat;
+	public class Ã¨{
+		public static void main(String[]ages){
+			/*Cat cat1=new Cat("¶þ¹·");
+			System.out.println(cat1.name);*/
+		
+	
+
+			/*Cat cat1=new Cat();
+			cat1.name="¶þ¹·";
+			cat1.age=2;
+			cat1.sex='ÄÐ';
+			cat1.type="Ã¨";*/
+			
+			
+			/*Cat cat1=new Cat();
+			cat1.setName("¶þ¹·");
+			cat1.setAge(2);
+			cat1.setSex('ÄÐ');
+			cat1.setType("Ã¨");
+			System.out.println(cat1.getName());*/
+			
+			
+			/*Cat cat1=new Cat();
+			cat1.setName("¶þ¹·");
+			cat1.setAge(2);
+			cat1.setSex('ÄÐ');
+			cat1.setType("Ã¨");
+			cat1.method();*/
+			
+			Cat cat=new Cat();
+			
+			
+			
+		}
+	}
+	
---
+++ src/练习2/Cat.java
@@ -0,0 +1,108 @@
+package 练习2;
+
+public class Cat {
+	/*public Cat(String name){
+		this.name=name;
+	}
+	public String name;
+	public int age;
+	public char sex;
+	public String type;*/
+	
+	
+	
+	/*public String name;
+	public int age;
+	public char sex;
+	public String type;*/
+	
+	
+	
+	/*private String name;
+	private int age;
+	private char sex;
+	private String type;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}*/
+	
+	
+	/*private String name;
+	private int age;
+	private char sex;
+	private String type;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public void method(){
+		System.out.println(name+"正在走猫步");
+	}*/
+	
+	
+	
+	/*public int a;
+	public int b;
+	public int add(int a,int b){
+		return a+b;
+	}
+	public float sdd(float a,float b) {
+		return a+b;
+	}*/
+	
+	
+	
+	/*public float a;
+	public float b;
+	public float abb(float a,float b) {
+		return a+b;
+	}*/
+	
+	
+	
+	
+	
+	
+	
+}
